3e2bb904c649ef759d0119fdf8dbea189ca4ba1e
Ondrej Sevcik <ondrej.sev@gmail.com>
2020-03-07 18:28:20 +0100

    Add charset

--- layout/layout.js
+++ layout/layout.js
@@ -10,6 +10,7 @@ module.exports = function(title, htmlContent) {
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="generator" content="" />
