b882c788d3fff82af8b40135aa7887951ecddd8b
Ondrej Sevcik <hi@ondrejsevcik.com>
2021-03-20 17:25:36 +0100

    Fix layout on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import { FullPageLayout } from "../components/full-page-layout"
 export default function HomePage() {
   return (
     <FullPageLayout>
-      <div className="flex justify-center">
-        <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 justify-items-center gap-8 mx-10 max-w-lg">
+      <div className="flex justify-center mx-10">
+        <div className="mt-12 inline-grid grid-cols-1 sm:grid-cols-2 gap-8">
           <img
-            className="rounded-full p-1 border-8 border-solid border-blue-100 w-56"
+            className="rounded-full p-1 border-8 border-solid border-blue-100 w-48 sm:w-56 justify-self-center sm:justify-self-end"
             alt="Profile picture"
             src="/images/profile-picture.jpg"
           />
 
-          <div>
+          <div className="max-w-xs">
             <div className="text-sm">Hi, I'm</div>
             <div className="text-2xl font-bold">Ondrej Sevcik</div>
             <p className="text-sm mt-2">
