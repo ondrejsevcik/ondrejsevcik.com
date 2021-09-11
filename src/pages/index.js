3ab1d9ba3195a18a6ab23b66863cf763f96af517
Ondrej Sevcik <hi@ondrejsevcik.com>
2021-09-11 11:05:39 +0200

    Update homepage text

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,28 +42,27 @@ export default function HomePage() {
   return (
     <FullPageLayout>
       <div className="flex justify-center mx-2">
+        {/* I would like to use Flexbox here, but Safari is missing gap support. I would have to hack a way around it. */}
         <div className="mt-12 inline-grid grid-cols-1 sm:grid-cols-2 gap-8">
           <img
             className="rounded-full p-1 border-8 border-solid border-blue-800 w-48 h-48 sm:w-56 sm:h-56 justify-self-center sm:justify-self-end"
             alt="Profile portrait"
             src="/images/profile-picture.jpg"
           />
-
-          <div className="max-w-xs">
-            <div className="text-sm">Hi, I'm</div>
-            <div className="text-2xl font-bold">Ondrej Sevcik</div>
-            <p className="mt-2 mr-2">
-              Frontend Developer with a special focus on simplicity and
-              well-designed apps. Currently building Cafe app at{" "}
-              <a className="link" href="https://cropster.com">
-                Cropster
-              </a>{" "}
-              and occasionally writing on my{" "}
-              <Link className="link" to="/blog">
-                /blog
-              </Link>
-              .
+          <div className="max-w-sm">
+            <div className="text-2xl font-bold">Hi, I'm Ondrej 👋🏼</div>
+            <p className="mt-2">
+              Frontend Developer, runner, minimalist, and occasional writer.
             </p>
+            <p className="mt-4">
+              I specialize in building UIs that <strong>do not break</strong>{" "}
+              and users <strong>love to use</strong>.
+            </p>
+            <p className="mt-4">
+              If you want to chat, feel free to write me an email or reach out
+              on Twitter.
+            </p>
+
             <div className="inline-grid grid-cols-5 gap-5 mt-6 text-xl">
               {socialLinks.map(socialLink => (
                 <span key={socialLink.href} className="w-6 h-6">
