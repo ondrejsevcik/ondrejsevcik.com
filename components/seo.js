d377ad2fdf79f9f0355055c522912b1668632010
Ondrej Sevcik <hi@ondrejsevcik.com>
2022-01-29 14:47:50 +0100

    Add favicon

--- components/seo.js
+++ components/seo.js
@@ -13,11 +13,28 @@ export function SearchEngineOptimization({
       <title>{[title, "Ondrej Sevcik"].filter(t => t).join(" | ")}</title>
       <meta name="description" content={description || defaultDescription} />
       <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
         rel="alternate"
         type="application/rss+xml"
         title="Blog posts RSS"
         href="https://ondrejsevcik.com/rss.xml"
-      ></link>
+      />
 
       {/* Open Graph data */}
       {article ? <meta property="og:type" content="article" /> : null}
