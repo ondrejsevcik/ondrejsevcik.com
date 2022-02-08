9a0a6d9f1b33840946cfaefb0f0a285cee873361
Ondrej Sevcik <hi@ondrejsevcik.com>
2022-02-08 18:36:23 +0100

    Add WarningBox component

--- components/blog-post-layout.js
+++ components/blog-post-layout.js
@@ -137,15 +137,6 @@ const BlogPostContent = styled.article`
     line-height: 1.25rem;
   }
 
-  & .warning-box {
-    border-top-right-radius: 0.25rem;
-    border-bottom-right-radius: 0.25rem;
-    border-left-width: 4px;
-    border-color: rgba(252, 214, 100, 1);
-    background-color: rgba(254, 241, 203, 1);
-    padding: 0.5rem;
-  }
-
   & .info-box {
     padding: 0.5rem;
     background-color: rgba(254, 241, 203, 1);
