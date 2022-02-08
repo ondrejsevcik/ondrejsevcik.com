a3ea72c1a7a777dec22c4a184bccf848d184ae91
Ondrej Sevcik <hi@ondrejsevcik.com>
2022-02-08 18:36:23 +0100

    Add WarningBox component

---
+++ components/blog/warning-box.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const WarningBox = styled.div`
+  border-top-right-radius: 0.25rem;
+  border-bottom-right-radius: 0.25rem;
+  border-left-width: 4px;
+  border-color: rgba(252, 214, 100, 1);
+  background-color: rgba(254, 241, 203, 1);
+  padding: 0.5rem;
+`
+
+export default WarningBox
