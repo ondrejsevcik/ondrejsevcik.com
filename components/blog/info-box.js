9ff31a987f1fd954d5a2ead313154953e9395a35
Ondrej Sevcik <hi@ondrejsevcik.com>
2022-02-08 18:40:57 +0100

    Add InfoBox component

---
+++ components/blog/info-box.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+// TODO: this is identical to WarningBox
+const InfoBox = styled.div`
+  border-top-right-radius: 0.25rem;
+  border-bottom-right-radius: 0.25rem;
+  border-left-width: 4px;
+  border-color: rgba(252, 214, 100, 1);
+  background-color: rgba(254, 241, 203, 1);
+  padding: 0.5rem;
+`
+
+export default InfoBox
