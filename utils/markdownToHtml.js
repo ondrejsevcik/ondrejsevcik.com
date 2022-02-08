64fdb55d8f675bd4c3ed738bfe7a2fc233280536
Ondrej Sevcik <hi@ondrejsevcik.com>
2022-02-08 18:27:26 +0100

    Remove unused packages

--- utils/markdownToHtml.js
+++
@@ -1,9 +0,0 @@
-import { remark } from "remark"
-import remarkHtml from "remark-html"
-
-export default async function markdownToHtml(markdown) {
-  const result = await remark()
-    .use(remarkHtml, { sanitize: false })
-    .process(markdown)
-  return result.toString()
-}
