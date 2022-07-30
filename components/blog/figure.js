1d5a4f1fbc536597cc073defc552b8ca23f259e4
Ondrej Sevcik <hi@ondrejsevcik.com>
2022-07-30 15:00:31 +0200

    Zoom in image on hover

--- components/blog/figure.js
+++ components/blog/figure.js
@@ -15,13 +15,26 @@ const FigCaption = styled.figcaption`
   line-height: 1.25rem;
 `
 
+const Img = styled.img`
+  border-radius: 4px;
+  transition-duration 0.2s;
+  transition-property: transform, box-shadow;
+
+  &:hover,
+  &:focus {
+    transform: scale(1.03);
+    --shadow: 0 4px 6px -1px rgb(0 0 0/0.1), 0 2px 4px -2px rgb(0 0 0/0.1);
+    box-shadow: 0 0 #0000, 0 0 #0000, var(--shadow);
+  }
+`
+
 function Figure({ src, alt, maxWidth, children }) {
   const caption = children || alt || null
 
   return (
     <FigureEl>
       <a href={src} style={{ maxWidth: maxWidth ?? "100%" }}>
-        <img src={src} alt={alt} />
+        <Img src={src} alt={alt} />
       </a>
       {caption ? <FigCaption>{caption}</FigCaption> : null}
     </FigureEl>
