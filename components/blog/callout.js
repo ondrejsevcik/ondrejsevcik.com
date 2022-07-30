e19af1c4d185d82376175086c7fe18c9758f24df
Ondrej Sevcik <hi@ondrejsevcik.com>
2022-07-30 12:17:03 +0200

    Add callout

---
+++ components/blog/callout.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  background-color: var(--blue-100);
+  padding: 1rem;
+  border-radius: 4px;
+  color: var(--gray-700);
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 0.75rem;
+`
+
+function Callout({ emoji, children }) {
+  return (
+    <Wrapper>
+      {emoji ? <div>{emoji}</div> : null}
+      <div>{children}</div>
+    </Wrapper>
+  )
+}
+
+export default Callout
