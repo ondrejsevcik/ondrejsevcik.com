07064b6eaea242086cfa5ab84e8f451cdae7b00f
Ondrej Sevcik <hi@ondrejsevcik.com>
2023-02-08 20:28:57 +0100

    Disable hydratation

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,3 +1,6 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+// Disable hydratation. It's not required for static pages.
+export const csr = false;
