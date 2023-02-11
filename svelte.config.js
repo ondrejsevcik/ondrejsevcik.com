75783a938cd11c1fac1428082716942fa2297176
Ondrej Sevcik <hi@ondrejsevcik.com>
2023-02-11 09:06:34 +0100

    Use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 
