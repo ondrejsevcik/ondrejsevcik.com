01dba85224218d87cddbfc6a436925c0aa93415e
Ondrej Sevcik <hi@ondrejsevcik.com>
2023-03-05 14:42:58 +0100

    Move posts into its own folders

--- utils/api.js
+++ utils/api.js
@@ -2,17 +2,16 @@ import fs from "fs"
 import { join } from "path"
 
 export async function getAllPostMeta() {
-  const blogPosts = fs
+  const slugs = fs
     .readdirSync(join(process.cwd(), "pages/blog"))
     .filter(f => f != "index.js")
     .filter(f => f != "index.module.css")
-    .map(fileName => fileName.replace(/\.mdx$/, ""))
 
   return await Promise.all(
-    blogPosts.map(blogPost =>
-      import(`../pages/blog/${blogPost}.mdx`).then(({ meta }) => ({
+    slugs.map(slug =>
+      import(`../pages/blog/${slug}/index.mdx`).then(({ meta }) => ({
         ...meta,
-        slug: blogPost,
+        slug,
       }))
     )
   )
