9e883def6a66c43d90872e2d482054e4d5b37fd0
Ondrej Sevcik <hi@ondrejsevcik.com>
2023-03-05 14:55:58 +0100

    Collocate and import statically imgs

--- components/blog/figure.js
+++ components/blog/figure.js
@@ -3,10 +3,11 @@ import styles from "./figure.module.css"
 
 function Figure({ src, alt, maxWidth, width, height, children }) {
   const caption = children || alt || null
+  const imgSrc = src?.src ? src.src : src // Next.js static import image
 
   return (
     <figure className={styles.figure}>
-      <a href={src} style={{ maxWidth: maxWidth ?? "100%" }}>
+      <a href={imgSrc} style={{ maxWidth: maxWidth ?? "100%" }}>
         <Image
           className={styles.img}
           src={src}
