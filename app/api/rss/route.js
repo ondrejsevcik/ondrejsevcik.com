aa007ba33153287f8156eb176dd4dadba07c164e
Ondrej Sevcik <hi@ondrejsevcik.com>
2023-11-17 17:51:52 +0100

    Move rss endpoint to app dir

---
+++ app/api/rss/route.js
@@ -0,0 +1,13 @@
+import { getRssFeedContent } from "../../../utils/getRssFeedContent"
+
+export async function GET(request) {
+  const feedContent = await getRssFeedContent()
+  const headers = new Headers(request.headers)
+  headers.set("Content-Type", "text/xml")
+  headers.set("Cache-Control", "public, max-age=0, must-revalidate")
+
+  return new Response(feedContent, {
+    status: 200,
+    headers,
+  })
+}
