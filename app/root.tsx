1d18ea4fc0fd4262c21d9f2055515aed9c260971
Ondrej Sevcik <hi@ondrejsevcik.com>
2024-02-09 15:06:28 +0200

    Move layout content to root

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import { cssBundleHref } from "@remix-run/css-bundle"
-import type { LinksFunction } from "@remix-run/node"
+import type { LinksFunction, MetaFunction } from "@remix-run/node"
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -9,8 +10,29 @@ import {
   ScrollRestoration,
 } from "@remix-run/react"
 
+import "./styles/tailwind-base.css"
+import "./styles/global.css"
+import "./styles/highlight-js.css"
+import styles from "./root.module.css"
+
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  [
+    ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+    // { rel: "stylesheet", href: tailwindStyles },
+    // { rel: "stylesheet", href: globalStyles },
+    // { rel: "stylesheet", href: highlightStyles },
+    // { rel: "stylesheet", href: rootStyles },
+  ],
+]
+
+export const meta: MetaFunction = () => {
+  return [{ title: `Ondrej Sevcik` }]
+}
+
+const items = [
+  { href: "/", title: "Home" },
+  { href: "/blog", title: "Blog" },
+  { href: "/notes", title: "Notes" },
 ]
 
 export default function App() {
@@ -19,11 +41,35 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Blogging about everything dev." />
+        <meta name="twitter:creator" content="@ondrejsevcik" />
+        <meta name="twitter:title" content="Ondrej Sevcik" />
+        <link rel="alternate" type="application/rss+xml" href="/rss.xml" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className={styles.layout}>
+        <nav className={styles.nav}>
+          {items.map(({ href, title }) => (
+            <Link key={href} href={href} className={styles.navLink}>
+              {title}
+            </Link>
+          ))}
+        </nav>
+
         <Outlet />
+
+        <footer className={styles.footerWrapper}>
+          &copy; {new Date().getFullYear()} Ondrej Sevcik
+        </footer>
+
+        {/* // TODO enable logging */}
+        {/* <Script
+          defer
+          data-domain="ondrejsevcik.com"
+          data-api="/hello/api/event"
+          src="/hello/js/script.js"
+        /> */}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
