9de11081ea56d111918f07bd6492143ca00b7544
Ondrej Sevcik <hi@ondrejsevcik.com>
2024-06-17 22:43:22 +0200

    Allow caching rss feed route

--- app/routes/rss[.xml]/route.ts
+++ app/routes/rss[.xml]/route.ts
@@ -2,10 +2,10 @@ import { getRssFeedContent } from "./getRssFeedContent"
 import type { LoaderFunctionArgs } from "@vercel/remix" // or cloudflare/deno
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const feedContent = await getRssFeedContent()
+  const feedContent = getRssFeedContent()
   const headers = new Headers(request.headers)
   headers.set("Content-Type", "text/xml")
-  headers.set("Cache-Control", "public, max-age=0, must-revalidate")
+  headers.set("Cache-Control", "public, max-age=3600")
 
   return new Response(feedContent, {
     status: 200,
