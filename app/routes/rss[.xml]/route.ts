f088efb1c3e8b5c73268f50b1dba6c57cf1a3bd6
Ondrej Sevcik <hi@ondrejsevcik.com>
2024-07-01 22:29:57 +0200

    Increase cache time for rss endpoint

--- app/routes/rss[.xml]/route.ts
+++ app/routes/rss[.xml]/route.ts
@@ -5,7 +5,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const feedContent = getRssFeedContent()
   const headers = new Headers(request.headers)
   headers.set("Content-Type", "text/xml")
-  headers.set("Cache-Control", "public, max-age=3600")
+  headers.set("Cache-Control", "public, max-age=86400, s-maxage=86400")
 
   return new Response(feedContent, {
     status: 200,
