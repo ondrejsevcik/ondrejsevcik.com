bd55a69bbdc285d2fb48627236526f19d60f2e95
Ondrej Sevcik <hi@ondrejsevcik.com>
2024-07-01 22:14:26 +0200

    Add vercel preset

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
+import { vercelPreset } from "@vercel/remix/vite"
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [remix()],
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+    }),
+  ],
 })
