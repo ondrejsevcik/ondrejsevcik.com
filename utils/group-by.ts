941b0da3ce1ed7a0ab02bde1774f5d87eb045605
Ondrej Sevcik <hi@ondrejsevcik.com>
2024-08-04 18:44:52 +0200

    add types to groupBy fn

--- utils/group-by.ts
+++ utils/group-by.ts
@@ -1,5 +1,5 @@
-export function groupBy(arr, getKeyFn) {
-	const groups = {};
+export function groupBy<T>(arr: T[], getKeyFn: (item: T) => string) {
+	const groups: Record<string, T[]> = {};
 	for (const el of arr) {
 		const key = getKeyFn(el);
 		if (!(key in groups)) {
